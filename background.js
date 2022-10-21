9dae01667a8422490560a1b7f99b2b20607f9648
N0H3dy <workmail2776@gmail.com>
2022-10-21 17:28:26 -0400

    Function to get selected text has been completed

--- background.js
+++ background.js
@@ -1,21 +1,19 @@
-var websocket;
 
-websocket = new WebSocket(host);
-console.log("======== websocket ===========", websocket);
-
-websocket.onopen = function () {
-    websocket.send("I exist!");
-};
-
-websocket.onmessage = function (event) {
-    var received_msg = JSON.parse(event.data);
-    var notificationOptions = {
-        type: "basic",
-        title: received_msg.title,
-        message: received_msg.message,
-        iconUrl: "extension-icon.png"
-    }
-    chrome.notifications.create("", notificationOptions);
-};
-
-websocket.onclose = function () { alert("==== web socket closed ======"); };
+chrome.runtime.onInstalled.addListener(function() {
+// creates a listner that listens for the context menu
+  chrome.contextMenus.create({
+  // creates the context menu item
+      "title": "Create Concept Map",
+      "contexts": ["selection"],
+      "id": "Concept-Map"
+  });
+});
+  
+chrome.contextMenus.onClicked.addListener(function(info) {
+// runs the code bellow when the context menu item is clicked
+  if (info.menuItemId == "Concept-Map" && info.selectionText){
+  // the if statement makes sure there is text selected before allowing for the text to be clicked
+    console.log(info.selectionText)
+    // prints selected text
+  }
+})
