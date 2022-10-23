0b2bce813ef1da69ee01a48a3d1e345d80044caa
rushia <jonasg.13452@gmail.com>
2022-10-23 17:19:45 -0400

    frontend work

--- extension/javascript/background.js
+++ extension/javascript/background.js
@@ -1,20 +1,20 @@
+const url = "localhost:8080/";
+
 chrome.runtime.onInstalled.addListener(function() {
-  // creates a listner that listens for the context menu
+  // creates a listener that listens for the context menu
     chrome.contextMenus.create({
     // creates the context menu item
         "title": "Create Concept Map",
         "contexts": ["selection"],
         "id": "Concept-Map"
     });
-  });
+});
     
-  chrome.contextMenus.onClicked.addListener(function(info) {
-  // runs the code bellow when the context menu item is clicked
-    if (info.menuItemId == "Concept-Map" && info.selectionText){
-      // the if statement makes sure there is text selected before allowing for the text to be clicked
-      const url = "localhost:5000/";
-      let xhttp = new XMLHttpRequest();
-      xhttp.open("GET", url, true);
-      xhttp.send();
-    }
-  })
+chrome.contextMenus.onClicked.addListener(function(info) {
+// runs the code bellow when the context menu item is clicked
+  if (info.menuItemId == "Concept-Map" && info.selectionText) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+});
