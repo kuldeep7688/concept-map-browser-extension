4cbcd38836b00c0f223e1aff558e67f880cf97c2
rushia <jonasg.13452@gmail.com>
2022-10-22 20:25:49 -0400

    mo's js scripts

--- extension/javascript/background.js
+++ extension/javascript/background.js
@@ -1,5 +1,18 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: "OFF",
+chrome.runtime.onInstalled.addListener(function() {
+  // creates a listner that listens for the context menu
+    chrome.contextMenus.create({
+    // creates the context menu item
+        "title": "Create Concept Map",
+        "contexts": ["selection"],
+        "id": "Concept-Map"
     });
   });
+    
+  chrome.contextMenus.onClicked.addListener(function(info) {
+  // runs the code bellow when the context menu item is clicked
+    if (info.menuItemId == "Concept-Map" && info.selectionText){
+    // the if statement makes sure there is text selected before allowing for the text to be clicked
+      console.log(info.selectionText)
+      // prints selected text
+    }
+  })
