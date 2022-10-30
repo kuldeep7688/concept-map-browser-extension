923185c1452f509f13574540164945262901e169
N0H3dy <workmail2776@gmail.com>
2022-10-30 12:21:24 -0400

    Backend basic structure finished, icons changed

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ from doctest import OutputChecker
 import json
 from flask import Flask, request, jsonify, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-     return render_template('popup.html')
+def home_page():
+    example_embed='This string is from python'
+    return render_template('index.html', embed=example_embed)
 
-@app.route('/test', methods=['POST'])
-
-def test():
-     output = request.get_json()
-     print(output)
-     print(type(output))
-     result = json.loads(output)
-     print(result)
-     print(type(result))
-     return result
-
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Python'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
 
 #Run the app:
 if __name__ == "__main__":
