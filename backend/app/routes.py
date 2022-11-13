2f38dd8bfe7f99f419123043846e18194f5ff240
rushia <jonasg.13452@gmail.com>
2022-11-13 17:21:40 -0500

    added sessions, get requests working woooo

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template, session
+
+import random
 
 routes = Blueprint("routes", __name__)
 
 @routes.route("/")
 def index():
-    return render_template('sample_network.html')
+    lol = random.randint(1, 10)
+    text = request.args.get('bro')
+    if text != None:
+        session['i'] = text
+    if 'i' in session:
+        text = session['i']
+    return render_template('sample_network.html', value=text, test=lol)
--- extension/src/index.js
+++ extension/src/index.js
@@ -1,22 +1,23 @@
-import axios from "axios";
-const api = "http://127.0.0.1:8000/";
+import axios from "axios"
+const api = "http://127.0.0.1:8000/"
 
-const form = document.querySelector(".form-data");
-const submittedText = document.querySelector(".text");
+const form = document.querySelector(".form-data")
 
 const generateMap = async text => {
+    if (text === "") { return }
     try {
-      const response = await axios.post(`${api}`, { text : text });
-      console.log("succeeded")
-    } catch (error) {
-        console.log("failed");
-    }
+        const response = await axios.get(`${api}`, { params: { bro: text } })
+        document.getElementById('conceptMap').src += '';
+        console.log("succeeded")
+        } catch (error) {
+        console.log("failed")
+        }
 };
 
 const handleSubmit = async e => {
-    e.preventDefault();
-    generateMap(submittedText.value);
-    console.log("submitted");
+    e.preventDefault()
+    generateMap(document.getElementById("text").value)
+    console.log("submitted")
 };
 
-form.addEventListener("submit", e => handleSubmit(e));
+form.addEventListener("submit", e => handleSubmit(e))
